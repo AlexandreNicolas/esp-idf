538f8b75aba9704099e23249f7cb65f5451cb0a0
Alexandre Seemund <alexandreseemund@gmail.com>
2019-02-10 17:26:20 -0200

    add app to project

---
+++ app/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Build this example, which does not use that standard IDF app template. See README.md for
+# more information about the build and how to run this example on the target once built.
+
+export PATH="$PATH:$HOME/Documents/esp-idf/xtensa-esp32-elf/bin"
+export IDF_PATH=/home/alexandre/Documents/esp-idf
+mkdir build; cd build
+cmake .. -DCMAKE_TOOLCHAIN_FILE=$IDF_PATH/tools/cmake/toolchain-esp32.cmake -DIDF_TARGET=esp32
+cmake --build .
